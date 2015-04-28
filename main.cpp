12a092bfad53879a0d4b5c41e525878cf0e69d29
finderskeepers <burak.guleryuz@ogr.sakarya.edu.tr>
2015-04-28 13:47:44 +0300

    infinity wateeeeergit add .!

---
+++ main.cpp
@@ -0,0 +1,18 @@
+//Şehirlerarası otobüslerde su dağıtma otomasyonu
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+cout << "Su alir miydiniz ? : ";
+
+cin.get();
+
+for(;;)
+
+cout << "Siz?\tSiz?" << endl;
+
+return 0;
+
+}
